bd9c2f974b019c0d58091a4baf4d7d9f283352f9
suzhang <sincesusu@163.com>
2016-07-28 16:22:00 +0800

    DateUtils

--- src/test/java/com/codestorm/j2sestudy/commons/lang3/time/DateUtilsTest.java
+++ src/test/java/com/codestorm/j2sestudy/commons/lang3/time/DateUtilsTest.java
@@ -1,5 +1,6 @@
 package com.codestorm.j2sestudy.commons.lang3.time;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import org.apache.commons.lang3.time.DateUtils;
@@ -10,8 +11,33 @@ public class DateUtilsTest {
 	public void test1() {
 		Date today = new Date();
 
-		Date dayAfTomorrow=DateUtils.addDays(today, 2);
-		
+		Date dayAfTomorrow = DateUtils.addDays(today, 2);
+
 		System.out.println(dayAfTomorrow);
 	}
+
+	@Test
+	public void test2() {
+		Date today = new Date();
+
+		try {
+			Date date=DateUtils.parseDate("2016-07-06 11:11:11", "yyyy-MM-dd HH:mm:ss");
+			System.out.println(date);
+		} catch (ParseException e) {
+			e.printStackTrace(); 
+		}
+	}
+	
+	@Test
+	public void test3() {
+		Date today = new Date();
+
+		try {
+			Date date=DateUtils.parseDate("2016-07-06 11:11:11", "yyyy-MM-dd HH:mm:ss");
+			System.out.println(date);
+		} catch (ParseException e) {
+			e.printStackTrace(); 
+		}
+	}
+
 }
