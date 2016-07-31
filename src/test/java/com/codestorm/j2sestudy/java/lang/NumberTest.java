76e8466efb31c090a90565f2cc8b7e4889b2b9f0
suzhang <sincesusu@163.com>
2016-07-31 23:17:17 +0800

    e

--- src/test/java/com/codestorm/j2sestudy/java/lang/NumberTest.java
+++ src/test/java/com/codestorm/j2sestudy/java/lang/NumberTest.java
@@ -6,23 +6,30 @@ import org.junit.Test;
 
 public class NumberTest {
 	@Test
-	public void test1(){
-		Number number=new Long("222");
-		
-		int intNumber=number.intValue();
+	public void test1() {
+		Number number = new Long("222");
+
+		int intNumber = number.intValue();
+
+		int result = Double.compare(12.3, 15);
+
+		System.out.println(result);
+
+		System.out.println(new Double(123).compareTo(new Double(144)));
 	}
-	
+
 	@Test
-	public void test2(){
-		Integer i=new Integer(123);
-		int afterReverseNumber=Integer.reverse(i);
+	public void test2() {
+		Integer i = new Integer(123);
+		int afterReverseNumber = Integer.reverse(i);
 		System.out.println(afterReverseNumber);
 	}
-	
+
 	@Test
-	public void test3(){
-		BigInteger big=new BigInteger("323222");
-		
+	public void test3() {
+		BigInteger big = new BigInteger("323222");
+
 		System.out.println(big);
+		
 	}
 }
